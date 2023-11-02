46fc7b94a8c783f423aa795a0aed15dae766032d
mupalvan <mupalvan@gmail.com>
2023-11-02 13:52:02 -0400

    add Account and Connection

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# import
+import os, logging,tracemalloc
 from telethon import TelegramClient
 
+#----------------------------------- logging --------------------------------------------
+logging.basicConfig(
+    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
+tracemalloc.start()
+#---------------------------- set info acc ----------------------------------------------
+api_id = 1331656
+api_hash = '868c8254ed0fbc05a5ef0dab474ffdf9'
+phone_number = '+989308756586'
+#------------------------------ connect client ------------------------------------------
+if not os.path.exists('session'):
+    os.makedirs('session')
+
+client = TelegramClient('session/'+phone_number, api_id, api_hash)
+client.start()
