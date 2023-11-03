8900e76b057727b1b1384210fdb618d936663235
mupalvan <mupalvan@gmail.com>
2023-11-03 07:45:08 -0400

    add

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def my_event_handler(event):
         infoList = [chat,sender,chat_id,sender_id]
         return infoList
     
-    print(info())
+    print(info(event)[0])
 
     if 'ping' in event.raw_text:
         await event.reply('Pong !!')
