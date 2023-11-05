0ddd664619ae143f6ba058611297b9184ea35810
mupalvan <mupalvan@gmail.com>
2023-11-05 05:47:55 -0500

    add

--- s.py
+++ s.py
@@ -1,9 +1,31 @@
+from datetime import datetime
 import instaloader
 
-# Get instance
 L = instaloader.Instaloader()
-USER = "fapezhgol"
-PASSWORD = "5540121341m"
-L.login(USER, PASSWORD)        # (login)
-L.interactive_login(USER)      # (ask password on terminal)
-L.load_session_from_file(USER) 
+
+posts = instaloader.Hashtag.from_name(L.context, "urbanphotography").get_posts()
+
+SINCE = datetime(2020, 5, 10)  # further from today, inclusive
+UNTIL = datetime(2020, 5, 11)  # closer to today, not inclusive
+
+k = 0  # initiate k
+#k_list = []  # uncomment this to tune k
+
+for post in posts:
+    postdate = post.date
+
+    if postdate > UNTIL:
+        continue
+    elif postdate <= SINCE:
+        k += 1
+        if k == 50:
+            break
+        else:
+            continue
+    else:
+        L.download_post(post, "#urbanphotography")
+        # if you want to tune k, uncomment below to get your k max
+        #k_list.append(k)
+        k = 0  # set k to 0
+
+#max(k_list)
