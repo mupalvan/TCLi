923c3fa28878e70ccfc2985c496e9697dd841517
mupalvan <mupalvan@gmail.com>
2023-11-05 05:37:19 -0500

    add

--- s.py
+++ s.py
@@ -5,6 +5,6 @@ L = instaloader.Instaloader()
 USER = "fapezhgol"
 PASSWORD = "5540121341m"
 # Optionally, login or load session
-L.two_factor_login(USER, PASSWORD)        # (login)
+L.login(USER, PASSWORD)        # (login)
 L.interactive_login(USER)      # (ask password on terminal)
 L.load_session_from_file(USER)
