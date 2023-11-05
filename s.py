a0be565581d5a6aa27dd39926fc74e9f184798b3
mupalvan <mupalvan@gmail.com>
2023-11-05 05:53:25 -0500

    add

--- s.py
+++ s.py
@@ -1,5 +1,7 @@
-from datetime import datetime
-import instaloader
+from instagrapi import Client
 
-L = instaloader.Instaloader()
-L.login("fapezhgol", "5540121341m")
+cl = Client()
+cl.login("fapezhgol", "5540121341m")
+
+user_id = cl.user_id_from_username("fapezhgol")
+medias = cl.user_medias(user_id, 20)
