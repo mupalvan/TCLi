f10fbb21a3a77f16085abd8c18aa2f8cc0722928
mupalvan <mupalvan@gmail.com>
2023-11-08 08:41:19 -0500

    add

--- s.py
+++ s.py
@@ -2,7 +2,7 @@ from instabot import Bot
 
 
 bot = Bot()
-bot.login(username="fapezhgol", password="5540121341m")
+bot.login(username="", password="")
 user_id = bot.get_user_id_from_username("lego")
 user_info = bot.get_user_info(user_id)
 print(user_info['biography'])
